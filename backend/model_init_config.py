0ce53737a9d602b9f1ad630194764ffe6872e077
kehuo <hkhuoke@hotmail.com>
2020-01-11 23:47:20 +0800

    update init func

--- backend/model_init_config.py
+++ backend/model_init_config.py
@@ -1,6 +1,6 @@
-from mobile_pay.init_func import init_all_csv_files
+from mobile_pay.init_func import init_mobile_pay_func
 
 
 global_init_func_dict = {
-    "mobile_pay": [init_all_csv_files]
+    "mobile_pay": [init_mobile_pay_func]
 }
